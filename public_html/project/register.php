f13f312de9f54d33b4e33d8b0c83de0f8fb0ed17
Emperor Gowdie <eg278@njit.edu>
2026-04-01 10:07:23 -0400

    updated register to save user data

--- public_html/project/register.php
+++ public_html/project/register.php
@@ -66,7 +66,18 @@ if (isset($_POST["email"], $_POST["password"], $_POST["confirm"])) {
     }
 
     if (!$hasError) {
-        echo "Success<br>";
+        // TODO 4: Hash password before storing
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+        $db = getDB(); // available due to the `require()` of `functions.php` 
+        // Code for inserting user data into the database
+        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES (:email, :password)");
+        try {
+            $stmt->execute([':email' => $email, ':password' => $hashed_password]);
+            echo "Successfully registered!";
+        } catch (Exception $e) {
+            echo "There was an error registering<br>"; // user-friendly message
+            error_log("Registration Error: " . var_export($e, true)); // log the technical error for debugging
+        }
     }
 }
 ?>
