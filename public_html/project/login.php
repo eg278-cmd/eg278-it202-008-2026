a254e8bbd0952a8b5f71cff7b23e55b8491279af
Emperor Gowdie <eg278@njit.edu>
2026-04-01 20:26:49 -0400

    added session logic for login, added logout and landing page

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -67,6 +67,8 @@ if (isset($_POST["email"], $_POST["password"])) {
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
                         echo "Welcome, $email!<br>";
+                        $_SESSION["user"] = $user; // add the data to the active session
+                        die(header("Location: landing.php"));
                     } else {
                         echo "Invalid password<br>";
                     }
