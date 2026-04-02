9a27e1263d8053ae64e3745f532b54e193c635e1
Emperor Gowdie <eg278@njit.edu>
2026-04-01 21:38:02 -0400

    added new helper functions and conditional html to nav.php

--- public_html/project/register.php
+++ public_html/project/register.php
@@ -40,8 +40,8 @@ if (isset($_POST["email"], $_POST["password"], $_POST["confirm"])) {
         $hasError = true;
     }
     // Sanitize and validate email
-    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $email = sanitize_email($email);
+    if (!is_valid_email($email)) {
         echo "Invalid email address<br>";
         $hasError = true;
     }
