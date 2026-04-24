446c73633ba725239586e9e7112cb92f4f1b0d02
Emperor Gowdie <eg278@njit.edu>
2026-04-24 18:43:24 -0400

    change user

--- public_html/project/admin/create_golf.php
+++ public_html/project/admin/create_golf.php
@@ -1,13 +1,11 @@
 <?php
 //note we need to go up 1 more directory
-require(__DIR__ . "/../../../lib/functions.php");
-
+require(__DIR__ . "/../../../partials/nav.php");
 
 if (!has_role("Admin")) {
     flash("You don't have permission to view this page", "warning");
     die(header("Location: " . get_url("landing.php")));
 }
-require(__DIR__ . "/../../../partials/nav.php");
 ?>
 
 <?php
