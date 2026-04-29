d7f5e06478e5070d251952225e89b36e9038fd3e
Emperor Gowdie <eg278@njit.edu>
2026-04-29 09:48:06 -0400

    debug line

--- public_html/project/admin/create_golf.php
+++ public_html/project/admin/create_golf.php
@@ -17,6 +17,12 @@ if (isset($_POST["action"])) {
     if ($action === "fetch") {
 
         $result = fetch_golf_schedule();
+        
+        echo "<pre>";
+        var_export($result);
+        echo "</pre>";
+        exit;
+
         error_log("Data from API: " . var_export($result, true));
 
         if ($result && isset($result["results"][0])) {
