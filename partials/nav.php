2740a32cd055d00c84fa5f6671d800f24895722f
Emperor Gowdie <eg278@njit.edu>
2026-04-29 21:13:40 -0400

    update nav 2

--- partials/nav.php
+++ partials/nav.php
@@ -1,14 +1,15 @@
 <?php
 require_once(__DIR__ . "/../lib/functions.php");
-//Note: this is to resolve cookie issues with port numbers
+
+// Resolve cookie issues with port numbers
 $domain = $_SERVER["HTTP_HOST"];
 if (strpos($domain, ":")) {
     $domain = explode(":", $domain)[0];
 }
-$localWorks = true; //some people have issues with localhost for the cookie params
-//if you're one of those people make this false
 
-//this is an extra condition added to "resolve" the localhost issue for the session cookie
+$localWorks = true;
+
+// Session cookie params
 if (($localWorks && $domain == "localhost") || $domain != "localhost") {
     session_set_cookie_params([
         "lifetime" => 60 * 60,
@@ -20,6 +21,7 @@ if (($localWorks && $domain == "localhost") || $domain != "localhost") {
         "samesite" => "lax"
     ]);
 }
+
 session_start();
 // DO NOT CLOSE PHP TAG
 ?>
@@ -27,21 +29,22 @@ session_start();
 <!-- include css and js files -->
 <link rel="stylesheet" href="<?php echo get_url('styles.css'); ?>">
 <script src="<?php echo get_url('helpers.js'); ?>"></script>
+
 <nav class="navbar">
     <ul>
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
             <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
         <?php endif; ?>
+
         <?php if (!is_logged_in()) : ?>
             <li><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
             <li><a href="<?php echo get_url('register.php'); ?>">Register</a></li>
         <?php endif; ?>
+
         <?php if (has_role("Admin")) : ?>
             <div class="dropdown">
-                <button class="dropbtn">Roles
-                    <i class="fa fa-caret-down"></i>
-                </button>
+                <button class="dropbtn">Roles</button>
                 <div class="dropdown-content">
 
                     <li><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
@@ -51,28 +54,27 @@ session_start();
                 </div>
             </div>
         <?php endif; ?>
+
         <?php if (has_role("Admin")) : ?>
             <div class="dropdown">
-                <button class="dropbtn">Stocks
-                    <i class="fa fa-caret-down"></i>
-                </button>
+                <button class="dropbtn">Stocks</button>
                 <div class="dropdown-content">
                     <li><a href="<?php echo get_url('admin/create_stock.php'); ?>">Create Stock</a></li>
                     <li><a href="<?php echo get_url('admin/list_stocks.php'); ?>">List Stock</a></li>
                 </div>
             </div>
         <?php endif; ?>
+
         <?php if (has_role("Admin")) : ?>
             <div class="dropdown">
-                <button class="dropbtn">Companies
-                    <i class="fa fa-caret-down"></i>
-                </button>
+                <button class="dropbtn">Companies</button>
                 <div class="dropdown-content">
                     <li><a href="<?php echo get_url('admin/create_company.php'); ?>">Create Company</a></li>
                     <li><a href="<?php echo get_url('admin/list_companies.php'); ?>">List Companies</a></li>
                 </div>
             </div>
         <?php endif; ?>
+
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
         <?php endif; ?>
@@ -130,7 +132,7 @@ session_start();
         position: absolute;
         background-color: #f9f9f9;
         min-width: 160px;
-        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
         z-index: 1;
     }
 
