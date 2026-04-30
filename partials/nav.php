490ef2d0a5c1c9e2f89d65213f1f463715cc5079
Emperor Gowdie <eg278@njit.edu>
2026-04-29 20:55:44 -0400

    fix list

--- partials/nav.php
+++ partials/nav.php
@@ -21,9 +21,9 @@ if (($localWorks && $domain == "localhost") || $domain != "localhost") {
     ]);
 }
 session_start();
-
-
+// DO NOT CLOSE PHP TAG
 ?>
+
 <!-- include css and js files -->
 <link rel="stylesheet" href="<?php echo get_url('styles.css'); ?>">
 <script src="<?php echo get_url('helpers.js'); ?>"></script>
